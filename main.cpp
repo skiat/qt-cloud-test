954df77522e01e0b3c049d385d01fba208bf4162
Luca Marchesi <skiat@skiat.ch>
2015-06-14 16:14:37 +0200

    use of ENV variable PORT to choose listening port

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    FakeHTTPServer myserver(8080);
+    quint16 port = 8080;
+    if (qEnvironmentVariableIsSet("PORT") && !qEnvironmentVariableIsEmpty("PORT")) {
+        port = qgetenv("PORT").toUInt();
+    }
+
+    if (qEnvironmentVariableIsSet("MAR_APP_ID") && !qEnvironmentVariableIsEmpty("MAR_APP_ID")) {
+        qDebug() << "Running on cloud MAR_APP_ID" << qgetenv("MAR_APP_ID");
+    }
+
+
+    FakeHTTPServer myserver(port);
 
     return a.exec();
 }
