299d40edb2a6c82bdf7315f4fae9fb5ecf20817e
Luca Marchesi <skiat@skiat.ch>
2015-12-12 15:07:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,15 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     quint16 port = 8080;
+    
+    // verify ENV var availability
     if (qEnvironmentVariableIsSet("PORT") && !qEnvironmentVariableIsEmpty("PORT")) {
         port = qgetenv("PORT").toUInt();
     }
-
+    
     if (qEnvironmentVariableIsSet("MAR_APP_ID") && !qEnvironmentVariableIsEmpty("MAR_APP_ID")) {
         qDebug() << "Running on cloud MAR_APP_ID" << qgetenv("MAR_APP_ID");
     }
-
 
     FakeHTTPServer myserver(port);
 
